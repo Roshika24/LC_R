ca4c51e4e930d98ee2dcb267edc60f9ad8043202
Roshika Mishra <roshikam.24@gmail.com>
2026-09-16 22:53:49 +0530

    Time: 123 ms (9.03%), Space: 48.7 MB (5.21%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String minWindow(String s, String t) {
+      if(s.length()<t.length())return "";
+      HashMap<Character,Integer> mp=new HashMap<>();
+      HashMap<Character,Integer> curr=new HashMap<>();
+      
+      for(int i=0;i<t.length();i++){
+        mp.put(t.charAt(i),mp.getOrDefault(t.charAt(i),0)+1);
+        curr.put(t.charAt(i),0);
+        
+      }
+      String ans="";
+      int min=Integer.MAX_VALUE;
+      int i=0;int j=0;
+      int f=0;
+      int len=Integer.MAX_VALUE;
+      while(i<s.length()){
+        if(mp.containsKey(s.charAt(i))){
+            int c=mp.get(s.charAt(i));
+        int p=curr.get(s.charAt(i))+1;
+        
+
+        if(p<=c){
+           f++;
+        }
+        curr.put(s.charAt(i),p);
+        
+           while(j<=i && f==t.length()){
+             if(mp.containsKey(s.charAt(j))){
+                 int hcur=mp.get(s.charAt(j));
+                 int cur=curr.get(s.charAt(j))-1; 
+                 curr.put(s.charAt(j),cur);
+                 if(cur<hcur){
+                    f--;
+                 }
+                 len=Math.min(len,i-j+1);
+                 if(len<min){
+                    min=len;
+                    ans=s.substring(j,i+1);
+                 }
+                 j++;
+             }
+             else j++;
+           }
+             //System.out.println(i+" "+j+" "+len+" "+f);
+           
+        }
+        i++;
+      }
+      System.out.println(len);
+      return ans;
+    }
+}
